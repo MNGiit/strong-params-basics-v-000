940451e812a7d8aed94d0291ef85f21e7d54416e
Mohammed Noori <kmanoorib@gmail.com>
2019-06-16 03:02:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,10 +12,9 @@ class Post < ActiveRecord::Base
     @post.update(post_params(:title))
     redirect_to post_path(@post)
   end
+  
    
   private
-   
-   
   # We pass the permitted fields in as *args;
   # this keeps `post_params` pretty dry while
   # still allowing slightly different behavior
