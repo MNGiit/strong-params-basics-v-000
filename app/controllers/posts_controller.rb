5a5cf4d604a3ff0e9acb04507781724c3b7d3772
Mohammed Noori <kmanoorib@gmail.com>
2019-06-16 03:45:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,15 +24,24 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  
-	  redirect_to post_path(@post)
-	end
-
+	# def update
+	#   @post = Post.find(params[:id])
+	#   @post.update(params["post"])
+	#   
+	#   redirect_to post_path(@post)
+	# end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params(:title))
+    redirect_to post_path(@post)
+  end
+  
 	def edit
 	  @post = Post.find(params[:id])
 	end
+	
+	
+	
 	
 end
