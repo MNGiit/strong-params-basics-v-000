d727a0c2bcee1fd01d51014fd0ad89c84521f498
Mohammed Noori <kmanoorib@gmail.com>
2019-06-16 03:08:33 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ActiveRecord::Base
     @post.save
     redirect_to post_path(@post)
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
    
   def update
     @post = Post.find(params[:id])
